38ce5f090b89024427e0027ff0f47f7c3df45a99
richardli1598 <richardli1598@gmail.com>
2023-10-05 15:48:03 +0800

    feat: add workflow controller
    Signed-off-by: richardli1598 <richardli1598@gmail.com>

--- pkg/controller/workflow.go
+++ pkg/controller/workflow.go
@@ -15,3 +15,114 @@ limitations under the License.
 */
 
 package controller
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"time"
+
+	"gorm.io/gorm"
+	"opennaslab.io/bifrost/pkg/api"
+	"opennaslab.io/bifrost/pkg/container"
+	"opennaslab.io/bifrost/pkg/customapi"
+	"opennaslab.io/bifrost/pkg/database"
+)
+
+type WorkflowQueue struct {
+	Workflow map[string]interface{}
+	mutex    sync.Mutex
+}
+
+var WFQueue *WorkflowQueue
+
+func InitWorkflowQueue() {
+	WFQueue = &WorkflowQueue{
+		Workflow: make(map[string]interface{}),
+		mutex:    sync.Mutex{},
+	}
+}
+
+func (w *WorkflowQueue) AddWorkflow(name string) {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+
+	w.Workflow[name] = nil
+}
+
+func (w *WorkflowQueue) Run() {
+	for {
+		w.mutex.Lock()
+		defer w.mutex.Unlock()
+		for name, _ := range w.Workflow {
+			requeue, err := w.Reconcile(name)
+			if err == nil && !requeue {
+				delete(w.Workflow, name)
+			}
+		}
+		time.Sleep(time.Second * 3)
+	}
+}
+
+func (w *WorkflowQueue) Reconcile(name string) (requeue bool, err error) {
+	db := database.GetWorkflowMode()
+	wf, err := db.GetWorkflow(name)
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return false, nil
+	}
+	if wf.Status.State == api.ConfigurationWorkflowStateDeleting {
+		requeue, err := w.DeleteWorkflow(wf)
+		if !requeue && err != nil {
+			err := db.DeleteWorkflow(name)
+			if err != nil {
+				return false, err
+			}
+		}
+	}
+	return false, nil
+}
+
+func (w *WorkflowQueue) DeleteWorkflow(wf *api.ConfigurationWorkflow) (requeue bool, err error) {
+	for index, step := range wf.DNSConfigurationSteps {
+		stepState := wf.Status.DNSConfigurationSteps[index]
+
+		if stepState.State == api.ConfigurationWorkflowStateRunning || stepState.State == api.ConfigurationWorkflowStateRunningSuccess {
+			err := container.DeleteContainer(stepState.ContainerId)
+			if err != nil {
+				return false, err
+			}
+		}
+
+		if stepState.State == api.ConfigurationWorkflowStateDeletingSuccess {
+			continue
+		}
+
+		if stepState.State == api.ConfigurationWorkflowStateDeleting {
+			containerState, exitCode, err := container.GetContainer(stepState.ContainerId)
+			if err != nil {
+				return false, err
+			}
+			if containerState == "exited" && exitCode == 0 {
+				stepState.State = api.ConfigurationWorkflowStateDeletingSuccess
+				continue
+			}
+		}
+
+		stepDef := customapi.GetDNSStepDefinition(step.Use)
+		if stepDef == nil {
+			return false, fmt.Errorf("dns step definition %s not found", step.Use)
+		}
+		id, err := container.CreateContainer(wf.Name, step.Name, stepDef.Image)
+		if err != nil {
+			return false, err
+		}
+		stepState.ContainerId = id
+		stepState.State = api.ConfigurationWorkflowStateDeleting
+	}
+
+	return false, nil
+}
+
+func (w *WorkflowQueue) UpdateWorkflow(name string) (*api.ConfigurationWorkflow, error) {
+	return nil, nil
+}
