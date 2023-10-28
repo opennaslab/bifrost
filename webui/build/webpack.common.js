dffd3b37decb23342903686dd06bb91ddd36e67a
曹华胜 <1126390761@qq.com>
2023-10-28 16:08:23 +0800

    初始化前端项目

---
+++ webui/build/webpack.common.js
@@ -0,0 +1,108 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+const os = require('os');
+const HappyPack = require('happypack');
+
+const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length });
+
+const srcDir = path.join(__dirname, '../src');
+const devMode = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: {
+    main: path.join(__dirname, '../src/main.js'),
+  },
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].[chunkhash:8].js',
+    // publicPath: "/",
+    chunkFilename: 'chunk/[name].[chunkhash:8].js',
+  },
+  module: {
+    rules: [
+      // {
+      //   test: /\.(js|jsx)$/,
+      //   include: [srcDir],
+      //   loader: 'eslint-loader',
+      //   enforce: 'pre',
+      //   options: {
+      //     fix: true,
+      //   },
+      // },
+      {
+        test: /\.(js|jsx)$/,
+        include: [srcDir],
+        exclude: /(node_modules|bower_components)/,
+        use: ['happypack/loader?id=happybabel'],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        use: ['url-loader'],
+        include: [srcDir],
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: ['url-loader'],
+        include: [srcDir],
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        use: ['url-loader'],
+        include: [srcDir],
+      },
+    ],
+  },
+  plugins: [
+    // 开启 happypack 的线程池
+    new HappyPack({
+      id: 'happybabel',
+      loaders: ['babel-loader?cacheDirectory=true'],
+      threadPool: happyThreadPool,
+      // cache: true,
+      verbose: true,
+    }),
+    new HtmlWebpackPlugin({
+      template: `${srcDir}/index.html`,
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: `${srcDir}/assets/images/nowthen.jpg`,
+          to: 'nowthen.jpg',
+        },
+      ],
+    }),
+    new AntdDayjsWebpackPlugin()
+  ],
+  resolve: {
+    alias: {
+      '@': srcDir,
+      '@pages': `${srcDir}/pages`,
+    },
+  },
+  // optimization: {
+  //   removeAvailableModules: true, // 删除已解决的chunk (默认 true)
+  //   removeEmptyChunks: true, // 删除空的chunks (默认 true)
+  //   mergeDuplicateChunks: true // 合并重复的chunk (默认 true)
+  // }
+};
---
+++ webui/build/webpack.dev.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const {merge} = require('webpack-merge');
+// const path = require('path');
+
+const commonConfig = require('./webpack.common');
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  // 开发环境本地启动的服务配置
+  devServer: {
+    port: 9001,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+    compress: true,
+    // 接口代理转发
+    proxy: {
+      '/testapi': {
+        target: 'https://www.easy-mock.com/mock/5dff0acd5b188e66c6e07329/react-template',
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: { '^/testapi': '' },
+      },
+    },
+  },
+  plugins: [ new webpack.HotModuleReplacementPlugin()],
+  devtool: 'eval-source-map',
+  // optimization: {
+  //   moduleIds: 'named',
+  // },
+});
