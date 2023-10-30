da38a75ff8458c57e8262a462a14b130332c6678
曹华胜 <1126390761@qq.com>
2023-10-30 11:42:30 +0800

    初始化项目，搭建基础脚手架

---
+++ webui/.prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  // overrides: [
+  //   {
+  //     files: '.prettierrc',
+  //     options: {
+  //       parser: 'json',
+  //     },
+  //   },
+  // ],
+};
